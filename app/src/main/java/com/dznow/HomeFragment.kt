f89087dc438d4b657dbe233a81f26705ae004b26
iahmedbacha <fi_ahmed_bacha@esi.dz>
2019-06-13 02:32:45 +0100

    feat: news list xml files

--- app/src/main/java/com/dznow/HomeFragment.kt
+++ app/src/main/java/com/dznow/HomeFragment.kt
@@ -1,12 +1,10 @@
 package com.dznow
 
-
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,6 +24,4 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
-
-
 }
