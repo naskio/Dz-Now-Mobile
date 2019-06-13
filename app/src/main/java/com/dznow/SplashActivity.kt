1929ae016c14e847f66ec83b1bd30e67c441c18a
Mehdi Nassim KHODJA <khodjamehdinassim@gmail.com>
2019-06-13 09:58:17 +0100

    Adding Splash Screen

---
+++ app/src/main/java/com/dznow/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.dznow
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    private final val SPLASH_TIME_OUT: Long = 3000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
