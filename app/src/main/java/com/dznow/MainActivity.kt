1a6401d9e5f6f4fcf11c485d13639ca625836da2
Mehdi Nassim KHODJA <khodjamehdinassim@gmail.com>
2019-06-13 09:58:32 +0100

    fix: topBar Title

--- app/src/main/java/com/dznow/MainActivity.kt
+++ app/src/main/java/com/dznow/MainActivity.kt
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 
+// for using ids directly without findViewById
+import kotlinx.android.synthetic.main.activity_main.*;
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var fManager: FragmentManager
@@ -19,21 +22,25 @@ class MainActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.navigation_home -> {
                 fManager.beginTransaction().hide(active).show(homeFragment).commit()
+                toolbarTitle.text = getString(R.string.app_name);
                 active = homeFragment
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_for_you -> {
                 fManager.beginTransaction().hide(active).show(forYouFragment).commit()
+                toolbarTitle.text = getString(R.string.title_for_you);
                 active = forYouFragment
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_browse -> {
                 fManager.beginTransaction().hide(active).show(browseFragment).commit()
+                toolbarTitle.text = getString(R.string.title_browse);
                 active = browseFragment
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_bookmarks -> {
                 fManager.beginTransaction().hide(active).show(bookmarksFragment).commit()
+                toolbarTitle.text = getString(R.string.title_bookmarks);
                 active = bookmarksFragment
                 return@OnNavigationItemSelectedListener true
             }
@@ -46,12 +53,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         fManager = supportFragmentManager
         active = homeFragment
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
-        fManager.beginTransaction().add(R.id.fragment_container, bookmarksFragment, "4").hide(bookmarksFragment)
+        fManager.beginTransaction().add(R.id.fragmentContainer, bookmarksFragment, "4").hide(bookmarksFragment)
             .commit()
-        fManager.beginTransaction().add(R.id.fragment_container, browseFragment, "3").hide(browseFragment).commit()
-        fManager.beginTransaction().add(R.id.fragment_container, forYouFragment, "2").hide(forYouFragment).commit()
-        fManager.beginTransaction().add(R.id.fragment_container, homeFragment, "1").commit()
+        fManager.beginTransaction().add(R.id.fragmentContainer, browseFragment, "3").hide(browseFragment).commit()
+        fManager.beginTransaction().add(R.id.fragmentContainer, forYouFragment, "2").hide(forYouFragment).commit()
+        fManager.beginTransaction().add(R.id.fragmentContainer, homeFragment, "1").commit()
     }
 }
