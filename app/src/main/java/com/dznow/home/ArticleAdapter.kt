89cadff081df8066151a40ec70b8a02e195e9da5
iahmedbacha <fi_ahmed_bacha@esi.dz>
2019-06-17 04:30:59 +0100

    feat: article content page added

--- app/src/main/java/com/dznow/home/ArticleAdapter.kt
+++ app/src/main/java/com/dznow/home/ArticleAdapter.kt
@@ -1,5 +1,6 @@
 package com.dznow.home
 
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.dznow.R
+import com.dznow.article.ArticleActivity
 import com.dznow.article.ArticleModel
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.RequestCreator
@@ -30,9 +32,10 @@ class ArticleAdapter (private val articles : List<ArticleModel>) : RecyclerView.
         holder.title.text = article.title
         holder.minutesRead.text = article.minutes_read.toString()
         Picasso.get().load(article.cover_url).into(holder.articleCover)
+        holder.article = article
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+    inner class ViewHolder(itemView: View, var article : ArticleModel? = null) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
         val sourceName : TextView
         val createdAt : TextView
         val title : TextView
@@ -44,14 +47,20 @@ class ArticleAdapter (private val articles : List<ArticleModel>) : RecyclerView.
             sourceName = itemView.tv_article_source_name
             createdAt =  itemView.tv_article_created_at
             title = itemView.tv_article_title
-            minutesRead = itemView.tv_article_created_at
+            minutesRead = itemView.tv_article_minutes_read
             articleCover = itemView.tv_article_cover
         }
 
         override fun onClick(view: View) {
-            Toast.makeText(view.context,
-                "Clicked Position = " + adapterPosition, Toast.LENGTH_SHORT)
-                .show()
+            val intent = Intent(view.context, ArticleActivity::class.java)
+            intent.putExtra("sourceName", article?.source?.name)
+            intent.putExtra("title", article?.title)
+            intent.putExtra("content", article?.content)
+            intent.putExtra("minutes_read", article?.minutes_read)
+            intent.putExtra("cover_url", article?.cover_url)
+            intent.putExtra("created_at", article?.created_at)
+            intent.putExtra("url", article?.url)
+            view.context.startActivity(intent)
         }
     }
 }
